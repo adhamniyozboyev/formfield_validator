2e408d8f11ed4fa5ca2bb25374a52fe067924778
adhamniyozboyev <adhamjonniyozboyev@gmail.com>
2023-05-01 20:09:32 +0500

    start theme

---
+++ lib/main.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  final formKey = GlobalKey<FormState>();
+  String name = "";
+
+  @override
+  Widget build(BuildContext context) {
+    final double height = MediaQuery.of(context).size.height;
+    final GlobalKey<ScaffoldState> _scaffolKey = GlobalKey<ScaffoldState>();
+    return Scaffold(
+      key: _scaffolKey,
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+      ),
+      body: Container(
+        padding: const EdgeInsets.only(left: 40, right: 40),
+        child: Form(
+            key: formKey,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [],
+            )),
+      ),
+    );
+  }
+}
